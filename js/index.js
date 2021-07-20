3be6154af9eba53d01a16aa87f725f8f371785ae
Human-Remains <84139608+Human-Remains@users.noreply.github.com>
2021-07-20 13:04:02 -0400

    index.js update

--- js/index.js
+++ js/index.js
@@ -6,12 +6,12 @@ const copyright = document.createElement('p')
 copyright.innerHTML = `Chris McCourt &copy; ${thisyear}`;
 footer.appendChild(copyright)
 
-// creat Skills section
+// create skills section
 let skills = ["JavaScript", "HTML", "R-Programming", "CSS"];
 let skillsSection = document.getElementById("skills");
 let skillsList = skillsSection.querySelector("ul");
 
-//generate skills list"
+//"generate skills list"
 for (let i = 0; i < skills.length; i++) {
     let skill = document.createElement("li");
     skill.innerHTML = skills[i];
