90647c774c365c36f4b8a3171215af05f7625110
Human-Remains <84139608+Human-Remains@users.noreply.github.com>
2021-08-10 22:24:15 -0400

    updated lesson4-5 flex CSS

--- js/index.js
+++ js/index.js
@@ -62,14 +62,19 @@ const entry = event.target.parentNode
     messageForm.reset();
 });
 
-// sidebar menu 
-//Set the width of the sidebar to 250px and the left margin of the page content to 250px 
+// sidebar menu //
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-};
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body
+//function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+ // }
+  
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white
+  //function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+ // */
