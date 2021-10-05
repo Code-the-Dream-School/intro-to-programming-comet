27df4488ecdc4ec5ee689fae246aed05ddb1057b
Crystal Scott <clcarson84@gmail.com>
2021-10-04 21:09:20 -0700

    Form Functioning
    Ready for showtime with future employers.

--- Portfolio Website/js/index.js
+++ Portfolio Website/js/index.js
@@ -3,11 +3,14 @@ const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
+copyright.innerHTML = `&copy; ${thisYear} Crystal Scott`;
+footer.appendChild(copyright);
 
 //Skills Section
 const skillsArray = ['HTML', 'CSS', 'JavaScript', 'jQuery', 'WordPress', 'ekklesia360', 'GitHub', 'Adobe Photoshop', 'Accessibility'];
 const skillsSection = document.getElementById('skills');
 const skillsList = skillsSection.getElementsByTagName('ul')[0];
+
 //Scroll Animation
 const scroll = window.requestAnimationFrame ||
             function(callback){ window.setTimeout(callback, 1000/60)};
@@ -41,8 +44,8 @@ function isElementInViewport(el) {
       rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
   );
 }
-//Navigation Toggle
 
+//Navigation Toggle
 document.getElementById('hamnav').addEventListener("click", function() {
 	const input = document.getElementById('hamburger');
 	input.checked = !input.checked;
@@ -54,15 +57,7 @@ document.getElementById('hamnav').addEventListener("click", function() {
 	}
 });
 
-
-
-//Message Section with Form
-const messageForm = document.getElementsByName('leave_message')[0];
-const messagesList = document.getElementById('messages_list');
-const messageSection = document.getElementById('messages');
-const submitButton = document.getElementById('submit_button');
-
-//Projects
+//Projects Section
 fetchData("https://api.github.com/users/cscott828/repos");
 
 function fetchData(url) {
@@ -98,11 +93,6 @@ function gitHubRequest(data){
 	}
 }
 
-
-//Footer Section- Copyright Current Year
-copyright.innerHTML = `&copy; ${thisYear} Crystal Scott`;
-footer.appendChild(copyright);
-
 //Skills Section- Add list items of skills
 for(let i = 0; i < skillsArray.length; i += 1){
 	let skill = document.createElement('li');
@@ -111,76 +101,26 @@ for(let i = 0; i < skillsArray.length; i += 1){
 	skillsList.appendChild(skill);
 }
 
-//Connect Section- Message Form
-function createLi(formName, formEmail, formMessage){
-	const li = document.createElement('li');
-	li.innerHTML = `<a href="mailto:${formEmail}">${formName} </a><span class="message">Message: ${formMessage}</span> `;
-	//Remove Button
-	const removeButton = document.createElement('button');
-	removeButton.innerText = 'Remove';
-	removeButton.setAttribute('type', 'button');
-	//Edit Button
-	const editButton = document.createElement('button'); 
-	editButton.innerText = 'Edit';
-	editButton.setAttribute('type', 'button');
-	//Appending
-	li.appendChild(editButton);
-	li.appendChild(removeButton);
-	messagesList.appendChild(li);
-	//Display Messages Section
-	messageSection.style.display = ''; 
-}
-
-	//Form Value Collection
-submitButton.addEventListener('click', (event) => {
-	event.preventDefault();
-	const formName = document.getElementById('name').value;
-	const formEmail = document.getElementById('email').value;
-	const formMessage = document.getElementById('message').value;
-	createLi(formName, formEmail, formMessage);
-	//Add to give email functionality
-//	window.open('mailto:clcarson84@gmail.com?subject=New Form Input &body=' + formName + '%0A' + formEmail + '%0A' + formMessage);
-	messageForm.reset();
-});
-
-	//Hide Messages Section if Empty
-if (messagesList.children.length == 0){
-	messageSection.style.display = 'none';
-}
-
-messagesList.addEventListener('click', (e) => {
-	if (e.target.tagName === 'BUTTON') {
-		const button = e.target;
-		const li = button.parentNode;
-		const ul = li.parentNode;
-		if (button.textContent === 'Remove') {
-			ul.removeChild(li);
-			if (messagesList.children.length == 0){
-				messageSection.style.display = 'none';
+//Form 
+const form = document.getElementById("my-form");
+	async function handleSubmit(event) {
+		event.preventDefault();
+		const status = document.getElementById("status");
+		const data = new FormData(event.target);
+		fetch(event.target.action, {
+			method: form.method,
+			body: data,
+			headers: {
+				'Accept': 'application/json'
 			}
-		} else if (button.textContent === 'Edit') {
-			const span = li.querySelector('span');
-			console.log(span);
-			const input = document.createElement('input');
-			input.className += 'input';
-			input.type = 'text';
-			input.value = span.textContent;
-			li.insertBefore(input, span);
-			li.removeChild(span);
-			button.textContent = 'Save';
-		} else if (button.textContent === 'Save') {
-			const input = li.querySelector('input');
-			const span = document.createElement('span');
-			span.textContent = input.value;
-			li.insertBefore(span, input);
-			li.removeChild(input);
-			button.textContent = 'Edit';
-		}
+		}).then(response => {
+			status.classList.add('success');
+			status.innerHTML = "Your message has been successfully sent.";
+			form.reset();
+		}).catch(error => {
+			status.classList.add('error');
+			status.innerHTML = "Oops! There was a problem submitting your form."
+		});
 	}
-});
-
-
-
-
-
+	form.addEventListener("submit", handleSubmit);
 
